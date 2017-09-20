1bfe191fffb4cc056d2b5788502401d013f9484c
AJ Leonard <AJ@Andrews-iMac.fios-router.home>
2017-09-20 02:16:23 -0400

    changed button to generate a random grid size

--- Assignment1/src/GUI.java
+++ Assignment1/src/GUI.java
@@ -6,6 +6,7 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.*;
 
 public class GUI extends JFrame {
 
@@ -38,66 +39,51 @@ public class GUI extends JFrame {
 
 
         //create panel for text input
-        JPanel textPanel = new JPanel();
-//      text field
-        JTextField matrixField = new JTextField("Enter matrix dimensions", 20);
-        matrixField.setToolTipText("Matrix dimensions");
-        matrixField.requestFocus();
-        textPanel.add(matrixField);
+        JPanel buttonPanel = new JPanel();
         //create the submit button and add it to the text panel
-        JButton submitButton = new JButton("Submit");
-        textPanel.add(submitButton);
+        JButton generateButton = new JButton("Generate");
+        buttonPanel.add(generateButton);
 
         //add the text panel to the main panel
-        mainPanel.add(textPanel);
+        mainPanel.add(buttonPanel);
 
 
 //      listen for submit button clicked
 //      add panel to the frame
-        submitButton.addActionListener(new ActionListener() {
+        generateButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 //              grab the text field contents
-                String textFieldString = matrixField.getText();
 //              check if the string is not valid
-                            if(!(textFieldString.equals("5")) && !(textFieldString.equals("7"))&&!(textFieldString.equals("9"))&&!(textFieldString.equals("11"))){
-                                System.out.println("Failure");
-                                return;
-                                // handle error case here with a error window?
-                                //TODO: pop up a window alerting the user of an error
-                            }else{
-                                //check if the user already pressed the submit button so we dont keep adding grids to the layout
-                                if(mainPanel.getComponentCount()>1){
-                                    //the user already added a grid so delete the grid and revalidate
-                                    Component[] comp = mainPanel.getComponents();
-                                    mainPanel.remove(comp[1]);
-                                    frame.revalidate();
-                                }
-                                // convert the inputted string to an integer
-                                int parsedTextFieldNumber = Integer.parseInt(textFieldString);
-                                //variables for max rows and columns
-                                int maxRows = parsedTextFieldNumber;
-                                int maxColumns = parsedTextFieldNumber;
-                                //create the grid of numbers
-                                //create panel for grid
-                                JPanel gridPanel = new JPanel();
-                                //set the grid layout for the grid panel using the converted input
-                                gridPanel.setLayout(new GridLayout(parsedTextFieldNumber,parsedTextFieldNumber,0,0));
-                                //add labels
-                                for(int i = 0;i<parsedTextFieldNumber;++i){
-                                    for(int j = 0;j<parsedTextFieldNumber;++j){
-                                        //create a label and add it to the layout
-                                        JLabel label = new JLabel("1",SwingConstants.CENTER);
-                                        //set the border for each cell
-                                        label.setBorder(BorderFactory.createLineBorder(Color.black));
-                                        //add the label to the grid
-                                        gridPanel.add(label);
-                                    }
-                                }
-                    mainPanel.add(gridPanel);
+                int n = generateRandomValue();
+                int maxRows = n;
+                int maxColumns = n;
+                //check if the user already pressed the submit button so we dont keep adding grids to the layout
+                if(mainPanel.getComponentCount()>1){
+                    //the user already added a grid so delete the grid and revalidate
+                    Component[] comp = mainPanel.getComponents();
+                    mainPanel.remove(comp[1]);
                     frame.revalidate();
                 }
-
+                //create the grid of numbers
+                //create panel for grid
+                JPanel gridPanel = new JPanel();
+                //set the grid layout for the grid panel using the converted input
+                gridPanel.setLayout(new GridLayout(maxRows,maxColumns,0,0));
+                //add labels
+                for(int i = 0;i<maxRows;++i){
+                    for(int j = 0;j<maxColumns;++j){
+                        //TODO: generate the valid grid number
+                        //create a label and add it to the layout
+                        JLabel label = new JLabel("1",SwingConstants.CENTER);
+                        //set the border for each cell
+                        label.setBorder(BorderFactory.createLineBorder(Color.black));
+                        //add the label to the grid
+                        gridPanel.add(label);
+                    }
+                }
+                mainPanel.add(gridPanel);
+                frame.revalidate();
             }
         });
         this.add(mainPanel);
@@ -107,18 +93,30 @@ public class GUI extends JFrame {
 //      show the window
         this.setVisible(true);
     }
+    public static int generateRandomValue(){
+        int[] array = new int[4];
+        array[0] = 5;
+        array[1] = 7;
+        array[2] = 9;
+        array[3] = 11;
+        int rand = (int)(Math.random()*4);
+        return array[rand];
+
+    }
     //get the grid number depending on the current row and column
-//    public static int generateGridNumber(int currentRow, int currentColumn){
-//        return 0;
-//    }
-//    public static int[][] create2DArray(int rows, int columns, ){
-//        int[][] array = new int[rows][columns];
-//        //TODO: implement array with generated grid numbers
-//        for(int i = 0; i<rows;++i){
-//            for(int j = 0;j<columns;++j){
-//                array[i][j] = generateGridNumber(i,j);
-//            }
-//        }
-//        return array;
-//    }
+    public static int generateGridNumber(int currentRow, int currentColumn){
+        return 0;
+    }
+    public static int[][] create2DArray(int rows, int columns){
+        int[][] array = new int[rows][columns];
+        //TODO: implement array with generated grid numbers
+        for(int i = 0; i<rows;++i){
+            for(int j = 0;j<columns;++j){
+                array[i][j] = generateGridNumber(i,j);
+            }
+        }
+        //set the goal cell to 0
+        array[rows-1][columns-1]=0;
+        return array;
+    }
 }
